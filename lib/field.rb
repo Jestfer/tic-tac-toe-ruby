702ff622f90e4359dfcdb505b6ada142b5ca7b30
Josué Estévez Fernández <jestfer6@gmail.com>
2018-03-31 11:49:57 +0100

    field class fully-tested, preparing player and field objects -> game

---
+++ lib/field.rb
@@ -0,0 +1,17 @@
+class Field
+  attr_reader :fields
+
+  def initialize
+    @fields = {
+      1 => '',
+      2 => '',
+      3 => '',
+      4 => '',
+      5 => '',
+      6 => '',
+      7 => '',
+      8 => '',
+      9 => '',
+    }
+  end
+end
---
+++ spec/field_spec.rb
@@ -0,0 +1,23 @@
+require 'field'
+
+describe Field do
+  subject(:ttt_fields) { described_class.new }
+
+  describe "#initialize" do
+    it "should initialize with 9 empty fields" do
+      expect(ttt_fields.fields).to eq(
+        {
+          1 => '',
+          2 => '',
+          3 => '',
+          4 => '',
+          5 => '',
+          6 => '',
+          7 => '',
+          8 => '',
+          9 => '',
+        }
+      )
+    end
+  end
+end
